8160a6e759a74d7cc0ac0caf8ad5460a16f9cc8e
Mat <mk_titan@hotmail.com>
2015-05-17 12:34:01 +0200

    add

---
+++ LightBulbs/LightSource.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using LightBulbs.LightingSystems;
+
+namespace LightBulbs
+{
+    public class LightSource
+    {   
+
+
+        public string SystemId { get; private set; }
+
+        public LightSource(ILightingSystem ls, string systemId)
+        {
+            LightingSystem = ls;
+            SystemId = systemId;
+        }
+
+        public bool On {
+            get { return LightingSystem.GetOn(this); }
+            set
+            {
+                if (value)
+                {
+                    LightingSystem.TurnOn(this);
+                }
+                else
+                {
+                    LightingSystem.TurnOff(this);
+                }
+            }
+        }
+
+        public Color Color
+        {
+            get { return LightingSystem.GetColor(this); }
+            set
+            {
+                LightingSystem.SetColor(this, value);
+            }
+        }
+
+        public double Level
+        {
+            get { return LightingSystem.GetLevel(this); }
+            set{ LightingSystem.SetLevel(this, value); }
+        }
+
+
+        public ILightingSystem LightingSystem { get; private set; }
+    }
+}
